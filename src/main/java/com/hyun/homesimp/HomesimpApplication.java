9f5c95c7381b2a6c637ed9bf5019c63225382010
와이프 <wipe@Hyun-Mac-mini.local>
2026-06-10 22:40:01 +0900

    second commit

---
+++ src/main/java/com/hyun/homesimp/HomesimpApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.homesimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyun/homesimp/HomesimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.homesimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
